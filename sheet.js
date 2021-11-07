2229bba24e3aaebe0133ae48239b7337e9a1072a
vzemaitis <viliuszem11@gmail.com>
2021-11-08 01:32:57 +0200

    Uzduotis #6.3

---
+++ sheet.js
@@ -0,0 +1,9 @@
+const numbers = [1, -2, 3, -4 ,5]
+
+const filteredNumbers = numbers.map(mapas => ([mapas]))
+
+
+console.log('-- Sukurti masyvą--')
+console.log(numbers)
+console.log('-- Panaudoti map ir sukurti naują masyvą --')
+console.log(filteredNumbers)
